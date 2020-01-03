c379a01546b2a29189dbaf55d9ec7a26126a15ae
Hakim, Mohammad Imaduddin <mohammad.hakim@stud.th-luebeck.de>
2020-01-03 15:40:41 +0100

    add method of producing probability matrix

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,8 +9,15 @@ public class Test {
 
         for(String move : moves) {
             System.out.println(move);
-            System.out.println(nextMovesOf(readText(marlinTrain), move));
+            System.out.println(nextMoveFrequencyOf(readText(marlinTrain), move));
         }
+
+        Map<String, Map<String, Integer>> nextMoveFrequencies = new HashMap<>();
+        for(String move : moves) {
+            nextMoveFrequencies.put(move, nextMoveFrequencyOf(readText(marlinTrain), move));
+        }
+        double[][] probability = markovMatrix(nextMoveFrequencies);
+        System.out.println(Arrays.deepToString(probability).replace("],", "\\\n"));
     }
 
     /**
@@ -37,15 +44,15 @@ public class Test {
     /**
      * Calculates the frequency of each next movements of a given movement from a list of movement sequences
      * @param sequences the sequences from which the frequency will be determined
-     * @param move the movement in question
+     * @param nextMoveFrequencies the movement in question
      * @return return the map of frequency of each next movement
      */
-    public static Map<String, Integer> nextMovesOf(List<String[]> sequences, String move) {
+    public static Map<String, Integer> nextMoveFrequencyOf(List<String[]> sequences, String nextMoveFrequencies) {
         Map<String, Integer> freq = new HashMap<>();
         for(String[] sequence : sequences) {
             String curr = null;
             for(String movement : sequence) {
-                if(curr != null && curr.equals(move)) {
+                if(curr != null && curr.equals(nextMoveFrequencies)) {
                     if(!freq.containsKey(movement)) {
                         freq.put(movement, 1);
                     } else {
@@ -56,6 +63,33 @@ public class Test {
             }
         }
         return freq;
+    }
+
+    /**
+     * Construct probability matrix of a movement being followed by a particular move
+     * @param matrix map of the grouped next move frequencies
+     * @return two dimensional array of the probability
+     */
+    public static double[][] markovMatrix(Map<String, Map<String, Integer>> matrix) {
+        String[] nextMoves = {"bill_use", "prey_contact", "open_mouth", "ingest", "leave"};
+        double[][] probability = new double[5][5];
+        int i = 0; //row number
+        for(String key : matrix.keySet()) {
+            int total = 0; //combined counts of occurrences for probability calculation
+            for(String key2 : matrix.get(key).keySet()) {
+                total += matrix.get(key).get(key2);
+            }
+            int i2 = 0; //column number
+            for(String move : nextMoves) {
+                if(matrix.get(key).containsKey(move)) {
+                    probability[i][i2++] = (double) matrix.get(key).get(move)/total;
+                } else {
+                    probability[i][i2++] = 0.0;
+                }
+            }
+            i++;
+        }
+        return probability;
     }
 }
 
